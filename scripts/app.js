e341f392f38e119b2a33a8950b6471f3e0ae239e
Medhavin <g.medhavin@gmail.com>
2016-01-24 20:59:49 +0530

    Complete Exercise: Angular Forms and Form Validation

--- scripts/app.js
+++ scripts/app.js
@@ -75,4 +75,31 @@ angular.module('confusionApp',[])
                 }
             ];
 
-        }]);
+        }])
+
+        .controller('ContactController', ['$scope', function($scope){
+          $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+          $scope.channels = [{value:"tel", label:"Tel."}, {value:"Email", label:"Email"}];
+          $scope.invalidChannelSelection = false;
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope){
+          $scope.sendFeedback = function(){
+            console.log($scope.feedback);
+
+            if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+            else {
+                $scope.invalidChannelSelection = false;
+                $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                                   agree:false, email:"" };
+                $scope.feedback.mychannel="";
+
+                $scope.feedbackForm.$setPristine();
+                console.log($scope.feedback);
+            }    
+          };
+        }])         
+;
