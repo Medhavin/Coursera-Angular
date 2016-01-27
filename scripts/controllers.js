829a91ff241cd8284163e753a3a2c0c0734d2cb1
Medhavin <g.medhavin@gmail.com>
2016-01-27 19:09:28 +0530

    Complete Exercise: Client-Server Communication using

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -8,7 +8,14 @@ angular.module('confusionApp')
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes= menuFactory.getDishes();
+            $scope.dishes= [];
+
+            menuFactory.getDishes()
+            .then(
+                function(response) {
+                    $scope.dishes = response.data;
+                }
+            );
 
                         
             $scope.select = function(setTab) {
@@ -70,9 +77,13 @@ angular.module('confusionApp')
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
-            
-            $scope.dish = dish;
+            $scope.dish = {};
+            menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                }
+            );
             
         }])
 
@@ -97,7 +108,15 @@ angular.module('confusionApp')
 
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
 
-            $scope.featureddish = menuFactory.getDish(0);
+            $scope.featureddish = {};
+
+            menuFactory.getDish(0)
+            .then(
+                function(response){
+                    $scope.featureddish = response.data;
+                    $scope.showDish = true;
+                }
+            );
 
             $scope.monthspromotion = menuFactory.getPromotion(0);
 
